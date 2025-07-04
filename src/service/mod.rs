a0a01677c0b827b725fb48339da22318d2cb2902
rustonbsd <rustonbsd@mailfence.com>
2025-07-05 00:14:55 +0200

    sync

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,18 +1,18 @@
 
 #[cfg(target_os = "linux")]
 mod linux;
-#[cfg(target_os = "linux")]
-use linux::install_service;
+
+mod windows;
 
 #[derive(Debug, Clone)]
 pub struct ServiceParams {
     pub ssh_port: u16,
 }
 
-pub fn install_service(service_params: ServiceParams) -> anyhow::Result<()> {
+pub async fn install_service(service_params: ServiceParams) -> anyhow::Result<()> {
     #[cfg(target_os = "linux")]
     {
-        return linux::install_service(service_params)
+        return linux::install_service(service_params).await
     }
     anyhow::bail!("Service mode is only supported on linux")
 }
