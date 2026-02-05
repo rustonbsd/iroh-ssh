1c2cd09d305503220ce5a4d1696e710102a4cf19
Luke <luke@maraud.earth>
2026-02-05 05:12:39 -0500

    created nix build files

---
+++ nix/package.nix
@@ -0,0 +1,50 @@
+{
+  lib,
+  stdenv,
+  rustPlatform,
+  fetchFromGitHub,
+  autoAddDriverRunpath,
+  installShellFiles,
+  writableTmpDirAsHomeHook,
+  versionCheckHook,
+  nix-update-script,
+}:
+
+rustPlatform.buildRustPackage (finalAttrs: {
+  pname = "iroh-ssh";
+  version = "0.2.8";
+
+  src = fetchFromGitHub {
+    owner = "LukeDSchenk";
+    repo = "iroh-ssh";
+    tag = finalAttrs.version;
+    hash = "sha256-hFPM+U88bb9lST1iE9shbjqOzEzC3qhLQAsOxxqv9Pg=";
+  };
+
+  cargoHash = "sha256-zsMz7bu6uGWXk1opE9yjPPeRcbspJgCe2RF4U50610w=";
+
+  nativeBuildInputs = [
+    autoAddDriverRunpath
+    installShellFiles
+  ];
+
+  doInstallCheck = true;
+  nativeInstallCheckInputs = [
+    versionCheckHook
+    writableTmpDirAsHomeHook
+  ];
+  versionCheckProgram = "${placeholder "out"}/bin/iroh-ssh";
+  versionCheckProgramArg = "version";
+
+  passthru = {
+    updateScript = nix-update-script { };
+  };
+
+  meta = {
+    description = "ssh without ip";
+    homepage = "https://github.com/LukeDSchenk/iroh-ssh";
+    license = lib.licenses.mit;
+    mainProgram = "iroh-ssh";
+  };
+})
+
